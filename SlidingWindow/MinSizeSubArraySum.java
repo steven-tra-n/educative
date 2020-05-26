98da44213e48d6b93d89f431ef85262bc6bab713
Steven Tran <steventran9102@gmail.com>
2020-05-26 17:25:52 -0400

    findMinSubArray first attempt

---
+++ SlidingWindow/MinSizeSubArraySum.java
@@ -0,0 +1,28 @@
+package SlidingWindow;
+
+public class MinSizeSubArraySum {
+    public static int findMinSubArray(int S, int[] arr){
+        int sum = 0;
+        int windowStart = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++){
+            sum += arr[windowEnd];
+
+            while(sum >= S){
+                minLength = Math.min(minLength, (windowEnd - windowStart) + 1); // +1 because windowEnd starts from 0
+                sum -= arr[windowStart]; // Slide the window along
+                windowStart++;
+            };
+        };
+
+        return minLength;
+    };
+};
+
+// int result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
+// System.out.println("Smallest subarray length: " + result);
+// result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 8 });
+// System.out.println("Smallest subarray length: " + result);
+// result = MinSizeSubArraySum.findMinSubArray(8, new int[] { 3, 4, 1, 1, 6 });
+// System.out.println("Smallest subarray length: " + result);
--- main.java
+++ main.java
@@ -2,9 +2,5 @@ import SlidingWindow.*;
 
 public class main {
     public static void main(String[] args){
-        int[] arr = {2, 1, 5, 1, 3, 2};
-        int k = 3;
-
-        System.out.println(SlidingWindow.MaxSumSubArrayOfSizeK.findMaxSumSubArray1(k, arr));
     };
 };
