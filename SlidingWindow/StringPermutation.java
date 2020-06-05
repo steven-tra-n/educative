9e4c8b4be85ace620b1f8d51e434ffb9f0bae911
Steven Tran <steventran9102@gmail.com>
2020-06-04 21:38:12 -0400

    findPermutation second attempt

--- SlidingWindow/StringPermutation.java
+++ SlidingWindow/StringPermutation.java
@@ -17,11 +17,11 @@ public class StringPermutation {
             char rightChar = str.charAt(windowEnd);
             
             if(charFrequency.containsKey(rightChar)){ // str char exists in map
-                charFrequency.put(rightChar, -1);
+                charFrequency.put(rightChar, -1); // Decrement char
             } else{
                 char leftChar = str.charAt(windowStart);
                 if(charFrequency.containsKey(leftChar)){
-                    charFrequency.put(leftChar, charFrequency.getOrDefault(leftChar, 0) + 1);
+                    charFrequency.put(leftChar, charFrequency.getOrDefault(leftChar, 0) + 1); // Put char back in
                 };
 
                 windowStart++;
@@ -34,4 +34,53 @@ public class StringPermutation {
             return false;
         }
     };
+
+    public static boolean findPermutation1(String str, String pattern) {
+        int windowStart = 0;
+        int matched = 0;
+        
+        HashMap<Character, Integer> charFrequency = new HashMap<Character, Integer>();
+
+        for(int i = 0; i < pattern.length(); i++){ // Populate the map
+            char patternChar = pattern.charAt(i);
+
+            charFrequency.put(patternChar, charFrequency.getOrDefault(patternChar, 0) + 1);
+        };
+        
+        for(int windowEnd = 0; windowEnd < str.length(); windowEnd++){
+            char rightChar = str.charAt(windowEnd);
+
+            if(charFrequency.containsKey(rightChar)){
+                charFrequency.put(rightChar, charFrequency.get(rightChar) - 1);
+
+                if(charFrequency.get(rightChar) == 0){
+                    matched++;
+                };
+            };
+
+            if(matched == charFrequency.size()){
+                return true;
+            };
+
+            if(windowEnd > pattern.length() - 1){ // Means str is now longer than pattern
+                windowStart++; // Shrink window
+
+                char leftChar = str.charAt(windowStart);
+                if(charFrequency.containsKey(leftChar)){
+                    if(charFrequency.get(leftChar) == 0){
+                        matched--; // Decrement since we are putting this char back into the map
+                    };
+
+                    charFrequency.put(leftChar, charFrequency.get(leftChar) + 1); // Put char back into the map
+                };
+            };
+        };
+
+        return false;
+    };
 };
+
+// System.out.println("Permutation exist: " + StringPermutation.findPermutation1("oidbcaf", "abc"));
+// System.out.println("Permutation exist: " + StringPermutation.findPermutation1("odicf", "dc"));
+// System.out.println("Permutation exist: " + StringPermutation.findPermutation1("bcdxabcdy", "bcdyabcdx"));
+// System.out.println("Permutation exist: " + StringPermutation.findPermutation1("aaacb", "abc"));
--- main.java
+++ main.java
@@ -2,9 +2,9 @@ import SlidingWindow.*;
 
 public class main {
     public static void main(String[] args){
-        System.out.println("Permutation exist: " + StringPermutation.findPermutation("oidbcaf", "abc"));
-        System.out.println("Permutation exist: " + StringPermutation.findPermutation("odicf", "dc"));
-        System.out.println("Permutation exist: " + StringPermutation.findPermutation("bcdxabcdy", "bcdyabcdx"));
-        System.out.println("Permutation exist: " + StringPermutation.findPermutation("aaacb", "abc"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation1("oidbcaf", "abc"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation1("odicf", "dc"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation1("bcdxabcdy", "bcdyabcdx"));
+        System.out.println("Permutation exist: " + StringPermutation.findPermutation1("aaacb", "abc"));
     };
 };
