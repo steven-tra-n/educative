2ee4791bd0479565b41d24f3bbd1b5fd080f9459
Tran <stetran@deloitte.com>
2020-06-25 17:43:19 -0400

    TwoPointers first attempt

--- TwoPointers/SortedArraySquares.java
+++ TwoPointers/SortedArraySquares.java
@@ -2,21 +2,38 @@ package TwoPointers;
 
 public class SortedArraySquares {
     public static int[] makeSquares(int[] arr) {
-        int[] squares = new int[arr.length];
-        int positiveIndex, leftPointer, rightPointer;
+        int length = arr.length;
+        int[] result = new int[length];
+        int left = 0;
+        int right = length - 1; // Need to subtract by one to match array indicies
+        int highestSquareIndex = length - 1;
+        int leftSquare, rightSquare;
 
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] >= 0){
-                positiveIndex = i;
-                leftPointer = i - 1;
-                rightPointer = i + 1;
-                
-                break;
+        while(left <= right){
+            leftSquare = arr[left] * arr[left];
+            rightSquare = arr[right] * arr[right];
+
+            if(leftSquare > rightSquare){
+                result[highestSquareIndex] = leftSquare;
+                left++;
+            } else{
+                result[highestSquareIndex] = rightSquare;
+                right--;
             };
+
+            highestSquareIndex--;
         };
 
-        
-
-        return squares;
+        return result;
     };
 };
+
+// int[] result = SortedArraySquares.makeSquares(new int[] { -2, -1, 2, 3 });
+// for (int num : result)
+// System.out.print(num + " ");
+// System.out.println();
+
+// result = SortedArraySquares.makeSquares(new int[] { -3, -1, 1, 2 });
+// for (int num : result)
+// System.out.print(num + " ");
+// System.out.println();
--- main.java
+++ main.java
@@ -4,12 +4,12 @@ import TwoPointers.*;
 
 public class main {
     public static void main(String[] args){
-        int[] result = SortedArraySquares.makeSquares(new int[] { -2, -1, 0, 2, 3 });
+        int[] result = SortedArraySquares.makeSquares(new int[] { -2, -1, 2, 3 });
         for (int num : result)
         System.out.print(num + " ");
         System.out.println();
 
-        result = SortedArraySquares.makeSquares(new int[] { -3, -1, 0, 1, 2 });
+        result = SortedArraySquares.makeSquares(new int[] { -3, -1, 1, 2 });
         for (int num : result)
         System.out.print(num + " ");
         System.out.println();
