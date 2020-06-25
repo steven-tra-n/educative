06e4ce96098d0e289451166aeeb6a38c83184518
Tran <stetran@deloitte.com>
2020-06-25 17:43:58 -0400

    TwoPointers added commented test cases

--- main.java
+++ main.java
@@ -4,14 +4,5 @@ import TwoPointers.*;
 
 public class main {
     public static void main(String[] args){
-        int[] result = SortedArraySquares.makeSquares(new int[] { -2, -1, 2, 3 });
-        for (int num : result)
-        System.out.print(num + " ");
-        System.out.println();
-
-        result = SortedArraySquares.makeSquares(new int[] { -3, -1, 1, 2 });
-        for (int num : result)
-        System.out.print(num + " ");
-        System.out.println();
     };
 };
