3b6ba5c3c083144e118485d858d27023714efff5
Steven Tran <steventran9102@gmail.com>
2020-07-11 16:10:40 -0400

    TripletWithSmallerSum first attempt

---
+++ TwoPointers/TripletWithSmallerSum.java
@@ -0,0 +1,30 @@
+package TwoPointers;
+
+import java.util.Arrays;
+
+public class TripletWithSmallerSum {
+    public static int searchTriplets(int[] arr, int target) {
+        int count = 0;
+        int left;
+        int right;
+
+        for(int i = 0; i < arr.length - 2; i++){
+            left = i + 1;
+            right = arr.length - 1;
+
+            while(left < right){
+                if(arr[i] + arr[left] + arr[right] < target){ 
+                    count += right - left; // Since array is ordered, any number between arr[right] and arr[left] will satisfy
+                    left++; // Since sum is less, increment left to see if next iteration also satisfies
+                } else{
+                    right--;
+                };
+            };
+        };
+
+        return count;
+    };
+};
+
+// System.out.println(TripletWithSmallerSum.searchTriplets(new int[] { -1, 0, 2, 3 }, 3));
+// System.out.println(TripletWithSmallerSum.searchTriplets(new int[] { -1, 4, 2, 1, 3 }, 5));
--- main.java
+++ main.java
@@ -2,8 +2,5 @@ import TwoPointers.*;
 
 public class main {
     public static void main(String[] args){
-        System.out.println(TripletSumCloseToTarget.searchTriplet(new int[] { -2, 0, 1, 2 }, 2));
-        System.out.println(TripletSumCloseToTarget.searchTriplet(new int[] { -3, -1, 1, 2 }, 1));
-        System.out.println(TripletSumCloseToTarget.searchTriplet(new int[] { 1, 0, 1, 1 }, 100));
     };
 };
