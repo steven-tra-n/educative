af818052f1a9c9009cc4533d654aacf5068c7b3e
Tran <stetran@deloitte.com>
2020-07-21 16:44:31 -0400

    DutchFlag first attempt

---
+++ TwoPointers/DutchFlag.java
@@ -0,0 +1,41 @@
+package TwoPointers;
+
+public class DutchFlag {
+    public static void sort(int[] arr) {
+        int low = 0; 
+        int high = arr.length - 1;
+
+        for(int i = low; i <= high;){
+            if(arr[i] == 0){ // All 0s will be swapped with low index
+                swap(arr, low, i);
+
+                i++;
+                low++;
+            } else if(arr[i] == 2){ // All 2s will be swapped with high index
+                swap(arr, high, i);
+
+                high--;
+            } else{ // All 1s will be inbetween 0s and 2s, no logic needed
+                i++;
+            };
+        };
+    };
+
+    private static void swap(int[] arr, int a, int b){
+        int placeHolder = arr[a];
+
+        arr[a] = arr[b];
+        arr[b] = placeHolder;
+    };
+};
+
+// int[] arr = new int[] { 1, 0, 2, 1, 0 };
+// DutchFlag.sort(arr);
+// for (int num : arr)
+//   System.out.print(num + " ");
+// System.out.println();
+
+// arr = new int[] { 2, 2, 0, 1, 2, 0 };
+// DutchFlag.sort(arr);
+// for (int num : arr)
+//   System.out.print(num + " ");
--- main.java
+++ main.java
@@ -2,6 +2,15 @@ import TwoPointers.*;
 
 public class main {
     public static void main(String[] args){
-        
+        int[] arr = new int[] { 1, 0, 2, 1, 0 };
+        DutchFlag.sort(arr);
+        for (int num : arr)
+          System.out.print(num + " ");
+        System.out.println();
+    
+        arr = new int[] { 2, 2, 0, 1, 2, 0 };
+        DutchFlag.sort(arr);
+        for (int num : arr)
+          System.out.print(num + " ");
     };
 };
