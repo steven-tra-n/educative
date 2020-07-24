8ecfcde9ff256a8847adf59f8f5522589b791ef6
Tran <stetran@deloitte.com>
2020-07-24 14:02:11 -0400

    BackspaceCompare second attempt

--- TwoPointers/BackspaceCompare.java
+++ TwoPointers/BackspaceCompare.java
@@ -4,19 +4,16 @@ public class BackspaceCompare {
     public static boolean compare(String str1, String str2) {
         int index1 = str1.length() - 1;
         int index2 = str2.length() - 1;
+        int tempIndex1;
+        int tempIndex2;
 
         while(index1 >= 0 || index2 >= 0){
-            if(str1.charAt(index1) == '#'){
-                index1 = index1 - 2;
-            };
+            tempIndex1 = findNextValidIndex(str1, index1);
+            tempIndex2 = findNextValidIndex(str2, index2);
 
-            if(str2.charAt(index2) == '#'){
-                index2 = index2 - 2;
-            };
-
-            if(str1.charAt(index1) == str2.charAt(index2)){
-                index1--;
-                index2--;
+            if(str1.charAt(tempIndex1) == str2.charAt(tempIndex2)){
+                index1 = tempIndex1 - 1; // Set indicies to newly found indicies
+                index2 = tempIndex2 - 1;
             } else{
                 return false;
             };
@@ -28,4 +25,27 @@ public class BackspaceCompare {
 
         return false;
     };
+
+    private static int findNextValidIndex(String str, int index){
+        int backspaceCount = 0;
+
+        while(index >= 0){
+            if(str.charAt(index) == '#'){
+                backspaceCount++;
+            } else if(backspaceCount > 0){
+                backspaceCount--; // Decrement backspaceCount as well as index since two chars need to be skipped for each '#'
+            } else{
+                break; // Index is valid, return
+            };
+
+            index--;
+        };
+
+        return index;
+    };
 };
+
+// System.out.println(BackspaceCompare.compare("xy#z", "xzz#"));
+// System.out.println(BackspaceCompare.compare("xy#z", "xyz#"));
+// System.out.println(BackspaceCompare.compare("xp#", "xyz##"));    
+// System.out.println(BackspaceCompare.compare("xywrrmp", "xywrrmu#p"));
