fb9942eb343976f79e27fbc087b419dbedd69c16
Steven Tran <steventran9102@gmail.com>
2020-07-25 18:02:39 -0400

    ShortestWindowSort first attempt

--- TwoPointers/ShortestWindowSort.java
+++ TwoPointers/ShortestWindowSort.java
@@ -4,11 +4,42 @@ public class ShortestWindowSort {
     public static int sort(int[] arr) {
         int left = 0;
         int right = arr.length - 1;
+        int rightMax = Integer.MIN_VALUE;
+        int leftMin = Integer.MAX_VALUE;
+        int result;
 
-        while(left < right && arr[left] <= arr[left + 1]){
-            left++;
+        while(left < arr.length - 1 && arr[left] <= arr[left + 1]){
+            left++; // Find starting index of subarray
         };
 
-        return - 1;
+        if(left == arr.length - 1){
+            return 0; // Array already sorted
+        };
+
+        while(right > 0 && arr[right] >= arr[right - 1]){
+            right--; // Find ending index of subarray
+        };
+
+        for(int i = left; i < right; i++){ // Find min and max of subarray
+            leftMin = Math.min(leftMin, arr[i]);
+            rightMax = Math.max(rightMax, arr[i]);
+        };
+
+        while(left > 0 && arr[left - 1] > leftMin){
+            left--; // In case the smallest number exists outside of subarray
+        };
+
+        while(right < arr.length - 1 && arr[right + 1] < rightMax){
+            right++; // In case the largest number exists outside of subarray
+        };
+
+        result = right - left + 1;
+
+        return result;
     };
 };
+
+// System.out.println(ShortestWindowSort.sort(new int[] { 1, 2, 5, 3, 7, 10, 9, 12 }));
+// System.out.println(ShortestWindowSort.sort(new int[] { 1, 3, 2, 0, -1, 7, 10 }));
+// System.out.println(ShortestWindowSort.sort(new int[] { 1, 2, 3 }));
+// System.out.println(ShortestWindowSort.sort(new int[] { 3, 2, 1 }));
