097555599e906fed5b9a94f20810102221ae5e1d
Steven Tran <steventran9102@gmail.com>
2020-07-26 15:35:29 -0400

    LinkedListCycle first attempt

---
+++ FastSlowPointers/ListNode.java
@@ -0,0 +1,10 @@
+package FastSlowPointers;
+
+public class ListNode {
+    int value = 0;
+    public ListNode next;
+
+    public ListNode(int value){
+        this.value = value;
+    };
+};
--- main.java
+++ main.java
@@ -1,6 +1,19 @@
-import TwoPointers.*;
+import FastSlowPointers.*;
 
 public class main {
     public static void main(String[] args){
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next.next;
+        System.out.println("LinkedList has cycle: " + LinkedListCycle.hasCycle(head));
     };
 };
