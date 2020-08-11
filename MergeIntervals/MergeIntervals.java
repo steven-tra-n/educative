baafa84912da018d22fe8fa475416f2acce0aa91
Tran <stetran@deloitte.com>
2020-08-11 14:22:37 -0400

    MergeIntervals draft

--- MergeIntervals/MergeIntervals.java
+++ MergeIntervals/MergeIntervals.java
@@ -1,6 +1,7 @@
 package MergeIntervals;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,10 +16,26 @@ public class MergeIntervals {
         Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
 
         // Loop through and find the lowest value when comparing two intervals
+        
+        Iterator<Interval> intervalItr = intervals.iterator();
+        Interval interval = intervalItr.next(); // Fetch first interval
+        int start = interval.start; // Initial start and end
+        int end = interval.end;
 
-        for(int i = 0; i < intervals.size(); i++){
-            System.out.println(intervals.get(i).start);
+        while(intervalItr.hasNext()){
+            interval = intervalItr.next();
+
+            if(interval.start <= end){ // Overlap found
+                end = Integer.max(interval.end, end); // We know start is already <= interval.start, since list is sorted on start. Find new end
+            } else{
+                mergedIntervals.add(new Interval(start, end)); // No overlap, add previous interval
+
+                start = interval.start; // Reset placeholders
+                end = interval.end;
+            };
         };
+
+        mergedIntervals.add(new Interval(start, end)); // Add last interval
 
         return mergedIntervals;
     };
