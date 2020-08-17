862fb58e59574cfbda0f930ecd497da27ba5aab8
Tran <stetran@deloitte.com>
2020-08-17 11:31:49 -0400

    InsertInterval first attempt

--- MergeIntervals/InsertInterval.java
+++ MergeIntervals/InsertInterval.java
@@ -2,8 +2,6 @@ package MergeIntervals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 public class InsertInterval {
@@ -15,19 +13,50 @@ public class InsertInterval {
         List<Interval> mergedIntervals = new ArrayList<>();
 
         int i = 0;
-        int start, end;
 
-        while(i < intervals.size() && intervals.get(i).end <= newInterval.start){ // Filter out intervals which end before newInterval start
+        while(i < intervals.size() && intervals.get(i).end < newInterval.start){ // Filter out intervals which end before newInterval start
             mergedIntervals.add(intervals.get(i++));
         };
 
-        while(i < intervals.size() && newInterval.start <= intervals.get(i).end){
-            start = Integer.min(newInterval.start, intervals.get(i).start);
-            end = Integer.max(newInterval.end, intervals.get(i).end);
+        while(i < intervals.size() && intervals.get(i).start <= newInterval.end){ // Intervals overlap at at least one point
+            newInterval.start = Integer.min(newInterval.start, intervals.get(i).start);
+            newInterval.end = Integer.max(newInterval.end, intervals.get(i).end);
 
             i++;
+        };
+
+        mergedIntervals.add(newInterval);
+
+        while(i < intervals.size()){
+            mergedIntervals.add(intervals.get(i++));
         };
 
         return mergedIntervals; 
     };
 };
+
+// List<Interval> input = new ArrayList<Interval>();
+// input.add(new Interval(1, 3));
+// input.add(new Interval(5, 7));
+// input.add(new Interval(8, 12));
+// System.out.print("Intervals after inserting the new interval: ");
+// for (Interval interval : InsertInterval.insert(input, new Interval(4, 6)))
+//   System.out.print("[" + interval.start + "," + interval.end + "] ");
+// System.out.println();
+
+// input = new ArrayList<Interval>();
+// input.add(new Interval(1, 3));
+// input.add(new Interval(5, 7));
+// input.add(new Interval(8, 12));
+// System.out.print("Intervals after inserting the new interval: ");
+// for (Interval interval : InsertInterval.insert(input, new Interval(4, 10)))
+//   System.out.print("[" + interval.start + "," + interval.end + "] ");
+// System.out.println();
+
+// input = new ArrayList<Interval>();
+// input.add(new Interval(2, 3));
+// input.add(new Interval(5, 7));
+// System.out.print("Intervals after inserting the new interval: ");
+// for (Interval interval : InsertInterval.insert(input, new Interval(1, 4)))
+//   System.out.print("[" + interval.start + "," + interval.end + "] ");
+// System.out.println();
