b4c7649de5a2e04b591e3ca81da63a2a23e1f72c
Tran <stetran@deloitte.com>
2020-08-18 17:28:26 -0400

    MinimumMeetingRooms draft

---
+++ MergeIntervals/Meeting.java
@@ -0,0 +1,11 @@
+package MergeIntervals;
+
+public class Meeting {
+    int start;
+    int end;
+
+    public Meeting(int start, int end) {
+        this.start = start;
+        this.end = end;
+    };
+};
---
+++ MergeIntervals/MinimumMeetingRooms.java
@@ -0,0 +1,9 @@
+package MergeIntervals;
+
+import java.util.List;
+
+public class MinimumMeetingRooms {
+    public static int findMinimumMeetingRooms(List<Meeting> meetings) {
+        return -1;
+    };
+};
