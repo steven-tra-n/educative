9c345f264999a8585b49ab9d0f4c73fcc2fa97b6
Steven Tran <steventran9102@gmail.com>
2020-08-19 11:27:10 -0400

    MinimumMeetingRooms

--- MergeIntervals/MinimumMeetingRooms.java
+++ MergeIntervals/MinimumMeetingRooms.java
@@ -1,9 +1,21 @@
 package MergeIntervals;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MinimumMeetingRooms {
     public static int findMinimumMeetingRooms(List<Meeting> meetings) {
-        return -1;
+        int rooms = 1;
+
+        Collections.sort(meetings, (a, b) -> Integer.compare(a.start, b.start));
+
+        for(int i = 1; i < meetings.size(); i++){
+            if(meetings.get(i).start < meetings.get(i - 1).end){
+                rooms++;
+                i = i + 2; // One room per two appointments
+            };
+        };
+
+        return rooms;
     };
 };
