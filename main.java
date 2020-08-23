e3121d7c3aeebd4b640a75e08b3e4716b083e226
Steven Tran <steventran9102@gmail.com>
2020-08-22 22:51:34 -0400

    MinimumMeetingRooms solution

--- main.java
+++ main.java
@@ -5,56 +5,5 @@ import MergeIntervals.*;
 
 public class main {
     public static void main(String[] args){
-        List<Meeting> input = new ArrayList<Meeting>() {
-          {
-              add(new Meeting(2, 3));
-              add(new Meeting(2, 4));
-              add(new Meeting(3, 5));
-              add(new Meeting(4, 5));
-          }
-        };
-        int result = MinimumMeetingRooms.findMinimumMeetingRooms(input);
-        System.out.println("Minimum meeting rooms required: " + result);
-    
-        input = new ArrayList<Meeting>() {
-          {
-            add(new Meeting(1, 4));
-            add(new Meeting(2, 5));
-            add(new Meeting(7, 9));
-          }
-        };
-        result = MinimumMeetingRooms.findMinimumMeetingRooms(input);
-        System.out.println("Minimum meeting rooms required: " + result);
-    
-        input = new ArrayList<Meeting>() {
-          {
-            add(new Meeting(6, 7));
-            add(new Meeting(2, 4));
-            add(new Meeting(8, 12));
-          }
-        };
-        result = MinimumMeetingRooms.findMinimumMeetingRooms(input);
-        System.out.println("Minimum meeting rooms required: " + result);
-    
-        input = new ArrayList<Meeting>() {
-          {
-            add(new Meeting(1, 4));
-            add(new Meeting(2, 3));
-            add(new Meeting(3, 6));
-          }
-        };
-        result = MinimumMeetingRooms.findMinimumMeetingRooms(input);
-        System.out.println("Minimum meeting rooms required: " + result);
-    
-        input = new ArrayList<Meeting>() {
-          {
-            add(new Meeting(4, 5));
-            add(new Meeting(2, 3));
-            add(new Meeting(2, 4));
-            add(new Meeting(3, 5));
-          }
-        };
-        result = MinimumMeetingRooms.findMinimumMeetingRooms(input);
-        System.out.println("Minimum meeting rooms required: " + result);
     };
 };
