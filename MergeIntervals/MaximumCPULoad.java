2a89b05a57f55e4f6b4e9e2a1d234a5a6cef3e27
Steven Tran <steventran9102@gmail.com>
2020-09-19 20:26:23 -0400

    MaximumCPULoad solution

--- MergeIntervals/MaximumCPULoad.java
+++ MergeIntervals/MaximumCPULoad.java
@@ -7,17 +7,35 @@ import java.util.PriorityQueue;
 public class MaximumCPULoad {
     public static int findMaxCPULoad(List<Job> jobs) {
         if(jobs == null || jobs.size() == 0){
-
+            return -1;
         };
-        PriorityQueue<Meeting> priorityJobs = 
+
+        Collections.sort(jobs, (a, b) -> Integer.compare(a.start, b.start)); // Sort jobs by start time
+        
+        int maxLoad = 0;
+        int currentLoad = 0;
+        PriorityQueue<Job> priorityJobs = 
             new PriorityQueue<>(jobs.size(), (a, b) -> Integer.compare(a.end, b.end));
 
         for(Job job : jobs){
-            while(!priorityJobs.isEmpty() && job.start >= priorityJobs.peek().start){
-                priorityJobs.poll();
+            while(!priorityJobs.isEmpty() && job.start >= priorityJobs.peek().end){ // Remove jobs that have ended
+                currentLoad -= priorityJobs.poll().cpuLoad;
             };
+
+            priorityJobs.offer(job);
+            currentLoad += job.cpuLoad;
+            maxLoad = Math.max(maxLoad, currentLoad);
         };
 
-        return -1;
+        return maxLoad;
     };
 };
+
+// List<Job> input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 3), new Job(2, 5, 4), new Job(7, 9, 6)));
+// System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
+
+// input = new ArrayList<Job>(Arrays.asList(new Job(6, 7, 10), new Job(2, 4, 11), new Job(8, 12, 15)));
+// System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
+
+// input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 2), new Job(2, 4, 1), new Job(3, 6, 5)));
+// System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
--- main.java
+++ main.java
@@ -6,13 +6,5 @@ import MergeIntervals.*;
 
 public class main {
     public static void main(String[] args){
-      List<Job> input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 3), new Job(2, 5, 4), new Job(7, 9, 6)));
-    System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
-
-    input = new ArrayList<Job>(Arrays.asList(new Job(6, 7, 10), new Job(2, 4, 11), new Job(8, 12, 15)));
-    System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
-
-    input = new ArrayList<Job>(Arrays.asList(new Job(1, 4, 2), new Job(2, 4, 1), new Job(3, 6, 5)));
-    System.out.println("Maximum CPU load at any time: " + MaximumCPULoad.findMaxCPULoad(input));
     };
 };
