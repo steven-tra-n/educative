bfc11ef15ce1f5b482fc5b2608a20d082ca54f48
Nhinh Dao <nhinhdao@andcultures-MacBook-Pro-2.local>
2021-05-15 19:31:31 -0400

    MinSizeSubArraySum revisit

--- SlidingWindow/MinSizeSubArraySum.java
+++ SlidingWindow/MinSizeSubArraySum.java
@@ -18,6 +18,27 @@ public class MinSizeSubArraySum {
 
         return minLength;
     };
+
+    public static int findMinSubArray2(int S, int[] arr){
+        int sum = 0;
+        int windowStart = 0;
+        int minLength = arr.length;
+
+        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++){
+            sum += arr[windowEnd];
+
+            while(sum >= S){
+                if((windowEnd - windowStart) < minLength){
+                    minLength = windowEnd - windowStart + 1;
+                };
+
+                sum -= arr[windowStart];
+                windowStart++;
+            };
+        };
+
+        return minLength;
+    };
 };
 
 // int result = MinSizeSubArraySum.findMinSubArray(7, new int[] { 2, 1, 5, 2, 3, 2 });
