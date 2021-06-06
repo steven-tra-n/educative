7e7799554d689bcbd49c0594f780dd64f82afb16
Nhinh Dao <nhinhdao@andcultures-MacBook-Pro-2.local>
2021-06-06 18:04:11 -0400

    MinimumWindowSubstring revisit

--- SlidingWindow/MinimumWindowSubstring.java
+++ SlidingWindow/MinimumWindowSubstring.java
@@ -50,8 +50,57 @@ public class MinimumWindowSubstring {
 
         return minLength > str.length() ? "" : str.substring(subStrStart, subStrStart + minLength);
     };
+
+    public static String findSubstring2(String str, String pattern) {
+        int windowStart = 0;
+        int matched = 0;
+        int minLength = str.length() + 1;
+        int subStrStart = 0;
+        char leftChar;
+        char rightChar;
+
+        HashMap<Character, Integer> patternHash = new HashMap<Character, Integer>();
+        StringBuilder resultString = new StringBuilder();
+
+        for (char c : pattern.toCharArray()) { // Populate hash
+            patternHash.put(c, patternHash.getOrDefault(c, 0) + 1);    
+        };
+
+        for(int windowEnd = 0; windowEnd < str.length(); windowEnd++){ // Iterate str
+            rightChar = str.charAt(windowEnd);
+
+            if(patternHash.containsKey(rightChar)){ // Char found in hash. Decrement hash count
+                patternHash.put(rightChar, patternHash.get(rightChar) - 1);
+
+                if(patternHash.get(rightChar) >= 0){ // Count each match
+                    matched++;
+                };
+            };
+
+            while(matched == pattern.length()){ // Full pattern match. Start to shrink window
+                if(minLength > windowEnd - windowStart + 1){ // minLength larger than window
+                    minLength = windowEnd - windowStart + 1;
+                    subStrStart = windowStart; // Set substring start index
+                };
+                
+                leftChar = str.charAt(windowStart);
+
+                if(patternHash.containsKey(leftChar)){
+                    if(patternHash.get(leftChar) == 0){ // Letter in pattern is no longer matched
+                        matched--;
+                    };
+
+                    patternHash.put(leftChar, patternHash.get(leftChar) + 1); // Increment hash
+                };
+
+                windowStart++;
+            };
+        };
+
+        return minLength > str.length() ? "" : str.substring(subStrStart, subStrStart + minLength);
+    };
 };
 
 // System.out.println(MinimumWindowSubstring.findSubstring("aabdec", "abc"));
-// System.out.println(MinimumWindowSubstring.findSubstring("abdabca", "abc"));
+// System.out.println(MinimumWindowSubstring.findSubstring("abdbca", "abc"));
 // System.out.println(MinimumWindowSubstring.findSubstring("adcad", "abc"));
