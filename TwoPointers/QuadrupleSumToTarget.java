3329a8795776003d37dfc02ffd6fed78267d5764
Steven Tran <steventran9102@gmail.com>
2021-08-04 19:22:58 -0400

    QuadrupleQuadrupleSumToTarget revisit

--- TwoPointers/QuadrupleSumToTarget.java
+++ TwoPointers/QuadrupleSumToTarget.java
@@ -54,6 +54,42 @@ public class QuadrupleSumToTarget {
             };
         };
     };
+
+    public static List<List<Integer>> searchQuadruplets2(int[] arr, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<>();
+        Arrays.sort(arr);
+
+        int outerSum;
+        int innerSum;
+        int difference;
+
+        for(int i = 0; i < arr.length - 3; i++){
+            for(int j = i + 1; j < arr.length - 2; j++){
+                int left = j + 1;
+                int right = arr.length - 1;
+
+                outerSum = arr[i] + arr[j];
+
+                while(left < right){
+                    innerSum = arr[left] + arr[right];
+                    difference = target - outerSum;
+
+                    if(innerSum == difference){
+                        quadruplets.add(Arrays.asList(arr[left], arr[right], arr[i], arr[j]));
+
+                        left++;
+                        right--;
+                    } else if(innerSum < difference){
+                        left++;
+                    } else{
+                        right--;
+                    };
+                };
+            };
+        };
+
+        return quadruplets;
+    };
 };
 
 // System.out.println(QuadrupleSumToTarget.searchQuadruplets(new int[] { 4, 1, 2, -1, 1, -3 }, 1));
