17e9ce0175c1fe466af3b56f56926552c922297e
steven-tra-n <steventran9102@gmail.com>
2021-11-12 16:24:21 -0500

    BitwiseXOR TwoSingleNumbers

--- BitwiseXOR/TwoSingleNumbers.java
+++ BitwiseXOR/TwoSingleNumbers.java
@@ -3,16 +3,28 @@ package BitwiseXOR;
 public class TwoSingleNumbers {
     public static int[] findSingleNumbers(int[] arr){
         int xor = 0;
+        int num1 = 0;
+        int num2 = 0;
 
+        // XOR the array to get num1 ^ num2
         for(int i = 0; i < arr.length; i++){
             xor ^= arr[i];
         };
 
+        // Shortcut to get rigth most set bit
+        int rightMostSetBit = xor & (-xor);
+
+        // Only one number in the array XORd with the rightMostSetBit will equal 0
+        // The other XORd will equal 1
         for(int i = 0; i < arr.length; i++){
-            xor ^= arr[i];
+            if((rightMostSetBit & arr[i]) != 0){
+                num1 ^= arr[i];
+            };
         };
 
-        return new int[] {xor, xor};
+        num2 ^= (num1 ^ xor);
+
+        return new int[] {num1, num2};
     };
 };
 
