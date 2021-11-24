a91d3cd9d3b5baa06c77db76870f092d16ad8b4d
steven-tra-n <steventran9102@gmail.com>
2021-11-24 13:25:34 -0500

    Test commit

---
+++ TopKElements/RearrangeString.java
@@ -0,0 +1,16 @@
+package TopKElements;
+
+public class RearrangeString {
+    public static String rearrangeString(String str){
+        // Add all characters into a hash map
+        // Add the hash map into a max heap
+        // Poll and append the key of the heap onto a running string
+        // Decrement the value of that key and offer it back to the heap
+
+        return "";
+    };
+};
+
+// System.out.println("Rearranged string: " + RearrangeString.rearrangeString("aappp"));
+//     System.out.println("Rearranged string: " + RearrangeString.rearrangeString("Programming"));
+//     System.out.println("Rearranged string: " + RearrangeString.rearrangeString("aapa"));
--- main.java
+++ main.java
@@ -1,7 +1,11 @@
-import CyclicSort.*;
+import java.util.List;
+
+import TopKElements.*;
 
 public class main {
     public static void main(String[] args){
-      
+      System.out.println("Rearranged string: " + RearrangeString.rearrangeString("aappp"));
+    System.out.println("Rearranged string: " + RearrangeString.rearrangeString("Programming"));
+    System.out.println("Rearranged string: " + RearrangeString.rearrangeString("aapa"));
     };
 };
